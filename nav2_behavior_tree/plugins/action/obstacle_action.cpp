62ef004dee5ad548bd3319ad3579c3cd7ca6f25d
Lorena Bajo Rebollo <lbajo9@gmail.com>
2020-01-26 21:21:22 +0100

    new dummy node obstacle_action added

--- nav2_behavior_tree/plugins/action/obstacle_action.cpp
+++ nav2_behavior_tree/plugins/action/obstacle_action.cpp
@@ -1,42 +1,84 @@
-// Copyright (c) 2018 Intel Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2020, Intelligent Robotics
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of Intelligent Robotics nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
 
-#ifndef NAV2_BEHAVIOR_TREE__OBSTACLE_ACTION_HPP_
-#define NAV2_BEHAVIOR_TREE__OBSTACLE_ACTION_HPP_
+/* Author: Lorena Bajo Rebollo lorena.bajo@urjc.es */
 
-#include <string>
+/* Author: Lorena Bajo Rebollo lorena.bajo@urjc.es */
+
+#ifndef NAV2_BEHAVIOR_TREE__OBSTACLE_DETECTED_ACTION_HPP_
+#define NAV2_BEHAVIOR_TREE__OBSTACLE_DETECTED_ACTION_HPP_
+
 #include <memory>
-#include <cmath>
-
+#include <string>
+#include "nav2_msgs/action/dummy_recovery.hpp"
 #include "nav2_behavior_tree/bt_action_node.hpp"
-//#include "nav2_msgs/action/back_up.hpp"
 
 namespace nav2_behavior_tree
 {
 
-class ObstacleAction : public BtActionNode<nav2_msgs::action::Obstacle>
+class ObstacleDetected : public BT::CoroActionNode
 {
 public:
-  ObstacleAction(
-    const std::string & xml_tag_name,
+  ObstacleDetected(
     const std::string & action_name,
     const BT::NodeConfiguration & conf)
-  : BtActionNode<nav2_msgs::action::Obstacle>(xml_tag_name, action_name, conf)
+  : BT::CoroActionNode(action_name, conf)
   {
-      std::cerr << "I can't pass. Can anyone help me?\n";  
+    node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
   }
 
+  BT::NodeStatus tick() override
+  {     
+    RCLCPP_WARN(node_->get_logger(), "Obstacle detected, Can you help me and move it?");
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  static BT::PortsList providedBasicPorts(BT::PortsList addition)
+  {
+    BT::PortsList basic = {
+      BT::InputPort<std::chrono::milliseconds>("server_timeout")
+    };
+    basic.insert(addition.begin(), addition.end());
+
+    return basic;
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts({});
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
 };
 
 }  // namespace nav2_behavior_tree
@@ -44,14 +86,8 @@ public:
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory)
 {
-  BT::NodeBuilder builder =
-    [](const std::string & name, const BT::NodeConfiguration & config)
-    {
-      return std::make_unique<nav2_behavior_tree::ObstacleAction>(
-        name, "obstacle", config);
-    };
-
-  factory.registerBuilder<nav2_behavior_tree::ObstacleAction>("Obstacle", builder);
+factory.registerNodeType<nav2_behavior_tree::ObstacleDetected>(
+    "ObstacleDetected");
 }
 
-#endif  // NAV2_BEHAVIOR_TREE__OBSTACLE_ACTION_HPP_
+#endif  // NAV2_BEHAVIOR_TREE__OBSTACLE_DETECTED_ACTION_HPP_
